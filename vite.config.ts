3d19848ae65892cfe09b52f53d5bbaf195b2f007
Piskun Ivan <tw1st3r111@mail.ru>
2024-12-25 14:00:57 +0300

    fix issues with Vite build

--- vite.config.ts
+++ vite.config.ts
@@ -1,30 +1,37 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 // https://vite.dev/config/
 export default defineConfig({
   base: './',
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@css': '/src/css',
-    },
-  },
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [{ src: 'src/css/images/**/*', dest: 'src/css/images' }],
+    }),
+  ],
   assetsInclude: ['**/*.svg'],
-  build: {
-    assetsDir: '',
-    rollupOptions: {
-      output: {
-        assetFileNames: assetInfo => {
-          if (/\.(woff2?|ttf|eot|otf)$/i.test(assetInfo.name ?? '')) {
-            return 'src/css/fonts/[name][extname]';
-          }
-          if (/\.(png|jpe?g|gif|svg|webp)$/i.test(assetInfo.name ?? '')) {
-            return 'src/css/images/[name][extname]';
-          }
-          return '[name][extname]';
-        },
-      },
-    },
-  },
+  // build: {
+  //   assetsDir: '',
+  //   rollupOptions: {
+  //     output: {
+  //       assetFileNames: assetInfo => {
+  //         if (/\.(woff2?|ttf|eot|otf)$/i.test(assetInfo.name ?? '')) {
+  //           return 'src/css/fonts/[name][extname]';
+  //         }
+
+  //         // if (/src\/css\/images\/partners\/$/i.test(assetInfo.name ?? '')) {
+  //         //   return 'src/css/images/partners/[name][extname]';
+  //         // }
+
+  //         // if (/\.(png|jpe?g|gif|svg|webp)$/i.test(assetInfo.name ?? '')) {
+  //         //   return 'src/css/images/[name][extname]';
+  //         // }
+
+  //         return '[name][extname]';
+  //       },
+  //     },
+  //   },
+  // },
 });
